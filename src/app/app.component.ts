dba9d1356c18143e4018a41a6d2592bb9d934970
athena272 <guilhermera272@gmail.com>
2025-01-20 03:15:05 -0300

    Update app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -13,5 +13,5 @@ import { SeparatorComponent } from './components/separator/separator.component';
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'angular-indexa';
+  alphabet: string = 'abcdefghijklmnopqrstuvwxyz'
 }
