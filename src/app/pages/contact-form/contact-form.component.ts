9e674263e772ea24312cd0779f6365e3f64c36e3
athena272 <guilhermera272@gmail.com>
2025-01-24 02:39:45 -0300

    Update contact-form.component.ts

--- src/app/pages/contact-form/contact-form.component.ts
+++ src/app/pages/contact-form/contact-form.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { ContainerComponent } from '../../components/container/container.component';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-contact-form',
   standalone: true,
-  imports: [ContainerComponent],
+  imports: [CommonModule, ContainerComponent],
   templateUrl: './contact-form.component.html',
   styleUrl: './contact-form.component.css'
 })
