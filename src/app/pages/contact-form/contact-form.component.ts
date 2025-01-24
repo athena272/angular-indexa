4afac5db99219c5f04c51a73d2884d048d071b54
athena272 <guilhermera272@gmail.com>
2025-01-24 04:18:42 -0300

    Update contact-form.component.ts

--- src/app/pages/contact-form/contact-form.component.ts
+++ src/app/pages/contact-form/contact-form.component.ts
@@ -2,14 +2,26 @@ import { Component } from '@angular/core';
 import { ContainerComponent } from '../../components/container/container.component';
 import { CommonModule } from '@angular/common';
 import { SeparatorComponent } from '../../components/separator/separator.component';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-contact-form',
   standalone: true,
-  imports: [CommonModule, ContainerComponent, SeparatorComponent],
+  imports: [CommonModule, ContainerComponent, SeparatorComponent, ReactiveFormsModule],
   templateUrl: './contact-form.component.html',
   styleUrl: './contact-form.component.css'
 })
 export class ContactFormComponent {
+  contactForm: FormGroup
 
+  constructor() {
+    this.contactForm = new FormGroup({
+      name: new FormControl(''),
+      telephone: new FormControl(''),
+      email: new FormControl(''),
+      birthDate: new FormControl(''),
+      socialNetworks: new FormControl(''),
+      observations: new FormControl('')
+    })
+  }
 }
