02f9e39b33488d8ee20bc1beacb45051601105d3
athena272 <guilhermera272@gmail.com>
2025-01-24 12:06:06 -0300

    update project files path

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,19 +1,7 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { RouterOutlet } from '@angular/router';
-import { FormsModule } from '@angular/forms';
-import { ContainerComponent } from './components/container/container.component';
-import { HeaderComponent } from './components/header/header.component';
-import { SeparatorComponent } from './components/separator/separator.component';
-import { ContactComponent } from './components/contact/contact.component';
-import agenda from './agenda.json'
+import { ContactListComponent } from './pages/contact-list/contact-list.component';
 import { ContactFormComponent } from './pages/contact-form/contact-form.component';
-
-interface Contact {
-  id: number
-  name: string
-  telephone: string
-}
 
 @Component({
   selector: 'app-root',
@@ -21,37 +9,12 @@ interface Contact {
   imports: [
     CommonModule,
     // RouterOutlet,
-    ContainerComponent,
-    HeaderComponent,
-    SeparatorComponent,
-    ContactComponent,
-    FormsModule,
+    ContactListComponent,
     ContactFormComponent
   ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  alphabet: string = 'abcdefghijklmnopqrstuvwxyz';
-  contacts: Contact[] = agenda
-  filterByText: string = ''
 
-  // Remove accents from a string
-  private removeAccents(text: string): string {
-    return text.normalize('NFD').replace(/[\u0300-\u036f]/g, '');
-  }
-
-  filterContactsByText(): Contact[] {
-    if (!this.filterByText) return this.contacts
-
-    return this.contacts.filter(contact => this.removeAccents(contact.name).toLowerCase().includes(this.filterByText.toLowerCase()))
-  }
-
-  filterContactsByInitialLetter(letter: string): Contact[] {
-    return this.filterContactsByText().filter(contact => this.removeAccents(contact.name).toLowerCase().startsWith(letter))
-  }
-
-  // filterContactsByInitialLetter(letter: string): Contact[] {
-  //   return this.contacts.filter(contact => contact.name.toLowerCase().startsWith(letter))
-  // }
 }
--- src/app/pages/contact-list/contact-list.component.ts
+++ src/app/pages/contact-list/contact-list.component.ts
@@ -1,12 +1,53 @@
 import { Component } from '@angular/core';
+import { ContainerComponent } from '../../components/container/container.component';
+import { HeaderComponent } from '../../components/header/header.component';
+import { SeparatorComponent } from '../../components/separator/separator.component';
+import { FormsModule } from '@angular/forms';
+import { ContactComponent } from '../../components/contact/contact.component';
+import { CommonModule } from '@angular/common';
+import agenda from './agenda.json'
+
+interface Contact {
+  id: number
+  name: string
+  telephone: string
+}
 
 @Component({
   selector: 'app-contact-list',
   standalone: true,
-  imports: [],
+  imports: [
+    CommonModule,
+    ContainerComponent,
+    HeaderComponent,
+    SeparatorComponent,
+    ContactComponent,
+    FormsModule,
+  ],
   templateUrl: './contact-list.component.html',
   styleUrl: './contact-list.component.css'
 })
 export class ContactListComponent {
+  alphabet: string = 'abcdefghijklmnopqrstuvwxyz';
+  contacts: Contact[] = agenda
+  filterByText: string = ''
 
+  // Remove accents from a string
+  private removeAccents(text: string): string {
+    return text.normalize('NFD').replace(/[\u0300-\u036f]/g, '');
+  }
+
+  filterContactsByText(): Contact[] {
+    if (!this.filterByText) return this.contacts
+
+    return this.contacts.filter(contact => this.removeAccents(contact.name).toLowerCase().includes(this.filterByText.toLowerCase()))
+  }
+
+  filterContactsByInitialLetter(letter: string): Contact[] {
+    return this.filterContactsByText().filter(contact => this.removeAccents(contact.name).toLowerCase().startsWith(letter))
+  }
+
+  // filterContactsByInitialLetter(letter: string): Contact[] {
+  //   return this.contacts.filter(contact => contact.name.toLowerCase().startsWith(letter))
+  // }
 }
